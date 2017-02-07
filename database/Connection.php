0c3fc940d244f87f3ff87e5b61dfa26976094b04
code-architect <codearchitectdeveloper@gmail.com>
2017-02-07 17:23:01 +0530

    bootstrapping the required files.
    creating a factory file(pattern).

--- database/Connection.php
+++ database/Connection.php
@@ -9,8 +9,24 @@ class Connection
             return new PDO('mysql:host=localhost;dbname=test_sub_project', 'root', '');
         }
         catch (PDOException $e) {
-            die(showPdoError($e));
+            die( self::showPdoError($e));
         }
+    }
+
+
+    /**
+     * Show PDO error
+     * @param $e
+     * @return string
+     */
+    public static function showPdoError($e)
+    {
+        $error = 'Could Not Connect: '.$e->getMessage().'<br>';
+        $error .= 'There is an error on line: '.$e->getLine().'<br>';
+        $error .= 'Error Number: '.$e->getCode().'<br>';
+        $error .= 'Error File Location: '.$e->getFile();
+
+        return $error;
     }
 
 }
--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
 
-require 'database/Connection.php';
-require 'database/QueryBuilder.php';
-include 'functions.php';
+$query = require 'bootstrap.php';
+
 require 'Task.php';
 
-$pdo = Connection::make();
-
-
-$query = new QueryBuilder($pdo);
 
 $articles = $query->selectAll('articals');
-
-
 
 require 'index.view.php';
